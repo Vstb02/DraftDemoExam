39c3369c0997f6138615809b0177028cfb3a247d
Vstb02 <v.steblev02@gmail.com>
2022-06-04 21:49:54 +0300

    Добавил контекст базы данных

---
+++ src/Core/Draft.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Draft.Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draft.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Material> Materials { get; set; }
+        DbSet<Supplier> Suppliers { get; set; }
+        DbSet<Storage> Storages { get; set; }
+        DbSet<TypeMaterial> TypeMaterials { get; set }
+        DbSet<TypeSupplier> TypeSupplier { get; set; }
+        DbSet<Unit> Units { get; set; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ src/Infrastructure/Draft.Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Draft.Application.Common.Interfaces;
+using Draft.Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draft.Persistence.Contexts
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<Material> Materials { get; set; } = null!;
+        public DbSet<Supplier> Suppliers { get; set; } = null!;
+        public DbSet<Storage> Storages { get; set; } = null!;
+        public DbSet<TypeMaterial> TypeMaterials { get; set; } = null!;
+        public DbSet<TypeSupplier> TypeSupplier { get; set; } = null!;
+        public DbSet<Unit> Units { get; set; } = null!;
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
