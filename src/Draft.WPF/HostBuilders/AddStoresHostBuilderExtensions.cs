d3e6194534421ff01321bd3f2682cdebcdf41c0a
Vstb02 <v.steblev02@gmail.com>
2022-06-05 15:19:32 +0300

    Подключил сервисы

---
+++ src/Draft.WPF/HostBuilders/AddStoresHostBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Draft.Application.Common.Interfaces.Services;
+using Draft.Domain.Entites;
+using Draft.Persistence.Services;
+using Draft.WPF.States.Accounts;
+using Draft.WPF.States.Authenticators;
+using Draft.WPF.States.Navigators;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draft.WPF.HostBuilders
+{
+    public static class AddStoresHostBuilderExtensions
+    {
+        public static IHostBuilder AddStores(this IHostBuilder host)
+        {
+            host.ConfigureServices(services =>
+            {
+                services.AddSingleton<INavigator, Navigator>();
+                services.AddSingleton<IAuthenticator, Authenticator>();
+                services.AddSingleton<IAccountStore, AccountStore>();
+            });
+
+            return host;
+        }
+    }
+}
--- src/Draft.WPF/HostBuilders/AddViewModelsHostBuilderExtensions.cs
+++ src/Draft.WPF/HostBuilders/AddViewModelsHostBuilderExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Draft.WPF.ViewModels;
+﻿using Draft.WPF.States.Authenticators;
+using Draft.WPF.States.Navigators;
+using Draft.WPF.ViewModels;
+using Draft.WPF.ViewModels.Factories;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -15,10 +18,42 @@ namespace Draft.WPF.HostBuilders
         {
             host.ConfigureServices(services =>
             {
+                services.AddTransient(CreateHomeViewModel);
                 services.AddTransient<MainViewModel>();
+
+                services.AddSingleton<CreateViewModel<HomeViewModel>>(services => () => services.GetRequiredService<HomeViewModel>());
+                services.AddSingleton<CreateViewModel<LoginViewModel>>(services => () => CreateLoginViewModel(services));
+                services.AddSingleton<CreateViewModel<RegisterViewModel>>(services => () => CreateRegisterViewModel(services));
+
+                services.AddSingleton<IDraftViewModelFactory, DraftViewModelFactory>();
+
+                services.AddSingleton<ViewModelDelegateRenavigator<HomeViewModel>>();
+                services.AddSingleton<ViewModelDelegateRenavigator<LoginViewModel>>();
+                services.AddSingleton<ViewModelDelegateRenavigator<RegisterViewModel>>();
             });
 
             return host;
+        }
+
+        private static HomeViewModel CreateHomeViewModel(IServiceProvider services)
+        {
+            return new HomeViewModel();
+        }
+
+        private static LoginViewModel CreateLoginViewModel(IServiceProvider services)
+        {
+            return new LoginViewModel(
+                services.GetRequiredService<IAuthenticator>(),
+                services.GetRequiredService<ViewModelDelegateRenavigator<HomeViewModel>>(),
+                services.GetRequiredService<ViewModelDelegateRenavigator<RegisterViewModel>>());
+        }
+
+        private static RegisterViewModel CreateRegisterViewModel(IServiceProvider services)
+        {
+            return new RegisterViewModel(
+                services.GetRequiredService<IAuthenticator>(),
+                services.GetRequiredService<ViewModelDelegateRenavigator<LoginViewModel>>(),
+                services.GetRequiredService<ViewModelDelegateRenavigator<LoginViewModel>>());
         }
     }
 }
