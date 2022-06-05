7d7fa798233cd975fcbe366ac7675d4924685a1c
Vstb02 <v.steblev02@gmail.com>
2022-06-05 09:24:24 +0300

    Подключил сервисы для работы с аккаунтом

--- src/Draft.WPF/HostBuilders/AddServicesHostBuilderExtensions.cs
+++ src/Draft.WPF/HostBuilders/AddServicesHostBuilderExtensions.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Draft.Application.Common.Interfaces.Services;
+using Draft.Application.Services;
+using Draft.Domain.Entites;
+using Draft.Persistence.Services;
+using Microsoft.AspNet.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +19,11 @@ namespace Draft.WPF.HostBuilders
         {
             host.ConfigureServices(services =>
             {
+                services.AddSingleton<IPasswordHasher, PasswordHasher>();
+
+                services.AddSingleton<IAuthenticationService, AuthenticationService>();
+                services.AddSingleton<IDataService<Account>, AccountDataService>();
+                services.AddSingleton<IAccountService, AccountDataService>();
             });
 
             return host;
