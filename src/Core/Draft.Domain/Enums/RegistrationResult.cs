2c087ae8008a979301574f86711c829d42e132c6
Vstb02 <v.steblev02@gmail.com>
2022-06-05 09:17:18 +0300

    Добавил состояния авторизации

---
+++ src/Core/Draft.Domain/Enums/RegistrationResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draft.Domain.Enums
+{
+    public enum RegistrationResult
+    {
+        Success,
+        PasswordsDoNotMatch,
+        EmailAlreadyExists,
+        UsernameAlreadyExists
+    }
+}
