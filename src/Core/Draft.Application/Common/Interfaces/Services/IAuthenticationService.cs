d9cf5dc8e4b54ff23755fb4b89228846bc3f3a8e
Vstb02 <v.steblev02@gmail.com>
2022-06-05 09:19:09 +0300

    Добавил сервис аутентификации

---
+++ src/Core/Draft.Application/Common/Interfaces/Services/IAuthenticationService.cs
@@ -0,0 +1,30 @@
+﻿using Draft.Domain.Entites;
+using Draft.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draft.Application.Common.Interfaces.Services
+{
+    public interface IAuthenticationService
+    {
+        /// <summary>
+        /// Регистрация нового пользователя
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="confirmPassword"></param>
+        /// <returns>Результат регистрации</returns>
+        Task<RegistrationResult> Register(string email, string username, string password, string confirmPassword);
+        /// <summary>
+        /// Получение аккаунта по данным пользователя
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns>Аккаунт пользователя</returns>
+        Task<Account> Login(string username, string password);
+    }
+}
---
+++ src/Core/Draft.Application/Services/AuthenticationService.cs
@@ -0,0 +1,74 @@
+﻿using Draft.Application.Common.Interfaces.Services;
+using Draft.Domain.Entites;
+using Draft.Domain.Enums;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draft.Application.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IAccountService _accountService;
+        private readonly IPasswordHasher _passwordHasher;
+
+        public AuthenticationService(IPasswordHasher passwordHasher,
+            IAccountService accountService)
+        {
+            _passwordHasher = passwordHasher;
+            _accountService = accountService;
+        }
+
+        public async Task<Account> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<RegistrationResult> Register(string email, string username, string password, string confirmPassword)
+        {
+            RegistrationResult result = RegistrationResult.Success;
+
+            if (password != confirmPassword)
+            {
+                result = RegistrationResult.PasswordsDoNotMatch;
+            }
+
+            Account emailAccount = await _accountService.GetByEmail(email);
+            if (emailAccount != null)
+            {
+                result = RegistrationResult.EmailAlreadyExists;
+            }
+
+            Account usernameAccount = await _accountService.GetByUsername(username);
+            if (usernameAccount != null)
+            {
+                result = RegistrationResult.UsernameAlreadyExists;
+            }
+
+            if (result == RegistrationResult.Success)
+            {
+                string hashedPassword = _passwordHasher.HashPassword(password);
+
+                User user = new User()
+                {
+                    Email = email,
+                    Username = username,
+                    PasswordHash = hashedPassword,
+                    DatedJoined = DateTime.Now
+                };
+
+                Account account = new Account()
+                {
+                    AccountHolder = user,
+                };
+
+                await _accountService.Create(account);
+            }
+
+            return result;
+        }
+    }
+}
