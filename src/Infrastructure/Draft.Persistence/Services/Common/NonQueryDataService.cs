90f0e16f40db0b19b06f38d0162fe961f651e242
Vstb02 <v.steblev02@gmail.com>
2022-06-05 09:18:46 +0300

    Добавил общие методы для работы с данными

---
+++ src/Infrastructure/Draft.Persistence/Services/Common/NonQueryDataService.cs
@@ -0,0 +1,58 @@
+﻿using Draft.Domain.Entites;
+using Draft.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draft.Persistence.Services.Common
+{
+    public class NonQueryDataService<T> where T : BaseEntity
+    {
+        private readonly ApplicationDbContextFactory _contextFactory;
+
+        public NonQueryDataService(ApplicationDbContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public async Task<T> Create(T entity)
+        {
+            using (ApplicationDbContext context = _contextFactory.CreateDbContext())
+            {
+                EntityEntry<T> createdResult = await context.Set<T>().AddAsync(entity);
+                await context.SaveChangesAsync();
+
+                return createdResult.Entity;
+            }
+        }
+
+        public async Task<T> Update(int id, T entity)
+        {
+            using (ApplicationDbContext context = _contextFactory.CreateDbContext())
+            {
+                entity.Id = id;
+
+                context.Set<T>().Update(entity);
+                await context.SaveChangesAsync();
+
+                return entity;
+            }
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            using (ApplicationDbContext context = _contextFactory.CreateDbContext())
+            {
+                T entity = await context.Set<T>().FirstOrDefaultAsync((e) => e.Id == id);
+                context.Set<T>().Remove(entity);
+                await context.SaveChangesAsync();
+
+                return true;
+            }
+        }
+    }
+}
---
+++ src/Infrastructure/Draft.Persistence/Services/GenericDataService.cs
@@ -0,0 +1,58 @@
+﻿using Draft.Application.Common.Interfaces.Services;
+using Draft.Domain.Entites;
+using Draft.Persistence.Contexts;
+using Draft.Persistence.Services.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draft.Persistence.Services
+{
+    public class GenericDataService<T> : IDataService<T> where T : BaseEntity
+    {
+        private readonly ApplicationDbContextFactory _contextFactory;
+        private readonly NonQueryDataService<T> _nonQueryDataService;
+
+        public GenericDataService(ApplicationDbContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+            _nonQueryDataService = new NonQueryDataService<T>(contextFactory);
+        }
+
+        public async Task<T> Create(T entity)
+        {
+            return await _nonQueryDataService.Create(entity);
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            return await _nonQueryDataService.Delete(id);
+        }
+
+        public async Task<T> Get(int id)
+        {
+            using (ApplicationDbContext context = _contextFactory.CreateDbContext())
+            {
+                T entity = await context.Set<T>().FirstOrDefaultAsync((e) => e.Id == id);
+                return entity;
+            }
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            using (ApplicationDbContext context = _contextFactory.CreateDbContext())
+            {
+                IEnumerable<T> entities = await context.Set<T>().ToListAsync();
+                return entities;
+            }
+        }
+
+        public async Task<T> Update(int id, T entity)
+        {
+            return await _nonQueryDataService.Update(id, entity);
+        }
+    }
+}
